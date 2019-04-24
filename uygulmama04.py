d1d62ebfc16336c16da28459d978607c3ea0e845
kaanozturk27 <48990486+kaanozturk27@users.noreply.github.com>
2019-04-24 14:23:33 +0300

    Create uygulmama04.py

---
+++ uygulmama04.py
@@ -0,0 +1,10 @@
+# Soru1
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+metin [0:21]
+
+# soru2
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık Veri", "Açık Kültür"]
+for x in liste :
+  print(x)
+  
+#soru3
